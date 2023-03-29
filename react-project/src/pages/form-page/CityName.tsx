ffd6ce9b0e26ed4fe01ab15500a47240e86ec1fe
novedice <Natalia.kitesurf@gmail.com>
2023-03-29 19:05:35 +0100

    refactor: rewrite form page with hooks and react forms

---
+++ react-project/src/pages/form-page/CityName.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const CityName = () => {
+  return (
+    <>
+      <label className="form-label">
+        City Name
+        {/* <input className="form-input-text" type="text" ref={this.cityInput}></input> */}
+      </label>
+      {/* {this.state.errors.cityError && (
+        <div className="form-errors">{this.state.errors.cityError}</div>
+      )} */}
+    </>
+  );
+};
+
+export default CityName;

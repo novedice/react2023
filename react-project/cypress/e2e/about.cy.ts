d5393b989bcbe784a0464dfa91fcbd07f8143cec
novedice <Natalia.kitesurf@gmail.com>
2023-05-01 22:34:07 +0100

    fix: fix configuration for coverage

--- react-project/cypress/e2e/about.cy.ts
+++ react-project/cypress/e2e/about.cy.ts
@@ -1,7 +1,6 @@
 describe('About page', () => {
   it('Visit About us page', () => {
-    cy.visit('http://localhost:5173');
-    cy.contains('about us').click();
+    cy.visit('http://localhost:5173/about');
     cy.url().should('include', '/about');
   });
 });
